ce477902e1e203d3c84a57b7becbe5c2595a30a1
jonasfalke@gmail.com <jonasfalke@gmail.com>
2021-03-18 16:30:27 +0100

    init commit

---
+++ Watch_project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Watch_project.c
+ *
+ * Created: 18-03-2021 16:26:32
+ * Author : Jonas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
